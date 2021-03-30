4092506d03ea45c5cdfb27f9abf66ffa58f706be
picnikW <52085125+picnikW@users.noreply.github.com>
2021-03-29 23:43:55 -0400

    Add files via upload

---
+++ MonitorAdjuster/src/ma/app/MonitorAdj.java
@@ -0,0 +1,33 @@
+package ma.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.stage.Stage;
+import ma.view.Controller;
+
+
+public class MonitorAdj extends Application {
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/sl/view/songLib.fxml"));
+		// main screen
+		SplitPane root = (SplitPane) loader.load();
+		Controller Controller = loader.getController();
+		Controller.start(primaryStage);
+
+		Scene scene = new Scene(root, 727, 467);
+	    primaryStage.setScene(scene);
+		primaryStage.setTitle("Song Library");
+		primaryStage.setResizable(false);
+		primaryStage.show();
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+}
---
+++ MonitorAdjuster/src/ma/view/Controller.java
@@ -0,0 +1,14 @@
+package ma.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Controller extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
